af6a70600a94ea63a74274097ea3994d27af01ca
maris.danne <maris.danne@gmail.com>
2021-09-30 09:49:17 +0300

    lapu lejupielade

--- skrapet.py
+++ skrapet.py
@@ -1,11 +1,20 @@
 import requests
+import time
 
 URL = 'https://www.ss.lv/lv/transport/cars/today/sell/'
 LAPAS = 'lapas/'
 
-rezultats = requests.get(URL)
-if rezultats.status_code == 200:
-    with open('1_lapa.html', 'w', encoding='UTF-8') as f:
-        f.write(rezultats.text)
-else:
-    print(f"ERROR: Statusa kods {rezultats.status_code}")
+def saglabat(url, datne):
+    rezultats = requests.get(url)
+    if rezultats.status_code == 200:
+        with open(f"{LAPAS}{datne}", 'w', encoding='UTF-8') as f:
+            f.write(rezultats.text)
+    else:
+        print(f"ERROR: Statusa kods {rezultats.status_code}")
+
+def lejupieladet_lapas(cik):
+    for i in range(1, cik + 1):
+        saglabat(f"{URL}/page{i}.html", f"{i}_lapa.html")
+        time.sleep(1)
+
+        
